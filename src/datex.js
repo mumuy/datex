9a15e967e185d8aad77966b52ff4c7a582fb7785
PASSER-BY <zhenghaole1990@126.com>
2023-05-06 20:05:55 +0800

    updated 添加语言包手工切换功能

--- src/datex.js
+++ src/datex.js
@@ -1,21 +1,21 @@
-let langMap = {};
-langMap['en-US'] = {
+let _langMap = {};
+_langMap['en-US'] = {
     'MMM':['Jan.','Feb.','Mar.','Apr.','May.','Jun.','Jul.','Aug.','Sept.','Oct.','Nov.','Dec.'],
     'MMMM':['January','February','March','April','May','June','July','August','September','October','November','December'],
     'Do':['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th','13th','14th','15th','16th','17th','18th','19th','20th','21st','22nd','23rd','24th','25th','26th','27th','28th','29th','30th','31st'],
     'W':['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
     'w':['Sun.','Mon.','Tues.','Wed.','Thur.','Fri.','Sat.'],
 };
-langMap['zh-CN'] = {
+_langMap['zh-CN'] = {
     'MMM':['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
     'MMMM':['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
     'Do':['1日','2日','3日','4日','5日','6日','7日','8日','9日','10日','11日','12日','13日','14日','15日','16日','17日','18日','19日','20日','21日','22日','23日','24日','25日','26日','27日','28日','29日','30日','31日'],
     'W':['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
     'w':['周日','周一','周二','周三','周四','周五','周六'],
 };
-let language = langMap['en-US'];
+let _lang = 'en-US';
 if(typeof self!='undefined'&&self.navigator){
-    language = langMap[self.navigator.language];
+    _lang = self.navigator.language;
 }
 const period = ['year','month','day','hour','minute','second','millsecond'];
 const initTime = [1970,1,1,0,0,0,0];
@@ -30,8 +30,19 @@ function isObject(value){
     return value != null && (typeof value == 'object' || typeof value == 'function');
 }
 
+datex.setLanguage = function(lang,data={}){
+    _langMap[lang] = Object.assign(_langMap[lang]||{},data);
+    return this;
+};
+datex.chooseLanguage = function(lang){
+    _lang = lang;
+    return this;
+};
+
 datex.prototype = {
     _date:null,
+    _langMap:{},
+    _lang:null,
     init:function(...argu){
         if(!argu.length){
             this._date = new Date();
@@ -67,6 +78,14 @@ datex.prototype = {
                 this._date.setFullYear(argu[0]);
             }
         }
+        return this;
+    },
+    setLanguage(lang,data={}){
+        this._langMap[lang] = Object.assign(this._langMap[lang]||{},data);
+        return this;
+    },
+    chooseLanguage(lang){
+        this._lang = lang;
         return this;
     },
     getTime(){
@@ -177,6 +196,8 @@ datex.prototype = {
             'x':$.timestamp,
             'Q':''+(~~($.month/3)),
         };
+        let langMap = Object.assign({},_langMap,this._langMap);
+        let language = langMap[this._lang||_lang];
         map['MMM'] = language['MMM'][$.month-1];
         map['MMMM'] = language['MMMM'][$.month-1];
         map['Do'] = language['Do'][$.day-1];
