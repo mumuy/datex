0fae1cacddbeae9d246f578eeca0fc05eeed68d9
PASSER-BY <zhenghaole1990@126.com>
2023-05-09 14:53:13 +0800

    updated 页面调整

--- src/datex.js
+++ src/datex.js
@@ -3,15 +3,15 @@ _langMap['en-US'] = {
     'MMM':['Jan.','Feb.','Mar.','Apr.','May.','Jun.','Jul.','Aug.','Sept.','Oct.','Nov.','Dec.'],
     'MMMM':['January','February','March','April','May','June','July','August','September','October','November','December'],
     'Do':['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th','13th','14th','15th','16th','17th','18th','19th','20th','21st','22nd','23rd','24th','25th','26th','27th','28th','29th','30th','31st'],
-    'W':['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-    'w':['Sun.','Mon.','Tues.','Wed.','Thur.','Fri.','Sat.'],
+    'WW':['Sun.','Mon.','Tues.','Wed.','Thur.','Fri.','Sat.'],
+    'WWW':['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'], 
 };
 _langMap['zh-CN'] = {
     'MMM':['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
     'MMMM':['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
     'Do':['1日','2日','3日','4日','5日','6日','7日','8日','9日','10日','11日','12日','13日','14日','15日','16日','17日','18日','19日','20日','21日','22日','23日','24日','25日','26日','27日','28日','29日','30日','31日'],
-    'W':['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
-    'w':['周日','周一','周二','周三','周四','周五','周六'],
+    'WW':['周日','周一','周二','周三','周四','周五','周六'],
+    'WWW':['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
 };
 let _lang = 'en-US';
 if(typeof self!='undefined'&&self.navigator){
@@ -195,15 +195,16 @@ datex.prototype = {
             'X':~~($.timestamp/1000),
             'x':$.timestamp,
             'Q':''+(~~($.month/3)),
+            'W':$.week
         };
         let langMap = Object.assign({},_langMap,this._langMap);
         let language = langMap[this._lang||_lang];
         map['MMM'] = language['MMM'][$.month-1];
         map['MMMM'] = language['MMMM'][$.month-1];
         map['Do'] = language['Do'][$.day-1];
-        map['W'] = language['W'][$.week];
-        map['w'] = language['w'][$.week];
-        return pattern.replace(/Y+|M+|D+|H+|h+|m+|s+|S+|Z+|Do|A|a|X|x|Q|W|w/g,function(key){
+        map['WW'] = language['WW'][$.week];
+        map['WWW'] = language['WWW'][$.week];
+        return pattern.replace(/Y+|M+|D+|H+|h+|m+|s+|S+|Z+|Do|A|a|X|x|Q|W+/g,function(key){
             return map[key]||'';
         });
     },
