5cfdc0e7146d9cb1dedeb61a63d9a5c7bca0dd36
zhenghaole <89932980@qq.com>
2024-04-28 19:14:21 +0800

    updated 构建工具调整

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-    presets: ['@babel/preset-env'],
+    presets: [
+        '@babel/preset-env'
+    ],
     plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-class-properties',
-        [
-            '@babel/plugin-syntax-import-attributes',
-            {
-                "deprecatedAssertSyntax": true
-            }
-        ]
+        '@babel/plugin-transform-runtime'
     ]
 }
