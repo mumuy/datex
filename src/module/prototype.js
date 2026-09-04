634f31f98f4d86f72cd52a70b5548d8a3ccd8f81
mumuy <zhenghaole1990@126.com>
2026-09-04 15:39:48 +0800

    updated 修复范围判断

--- src/module/prototype.js
+++ src/module/prototype.js
@@ -7,57 +7,63 @@ export default {
     _date:null,
     _offset:0,
     parse:function(...argu){
-        let param = argu.slice(0);
-        if(param.length&&(param[0]||param[0]===0)){
-            if(Object.getPrototypeOf(param[0])==Object.getPrototypeOf(this)){
-                this._date = param[0].toDate();
-                return param[0];
-            }else if(isDate(param[0])){
-                this._date = param[0];
+        let params = argu.slice(0);
+        if(params.length&&(params[0]||params[0]===0)){
+            if(Object.getPrototypeOf(params[0])==Object.getPrototypeOf(this)){
+                this._date = params[0].toDate();
+                return params[0];
+            }else if(isDate(params[0])){
+                this._date = params[0];
             }else{
                 // 参数映射
-                if(isArray(param[0])){
-                    param = periodValue.map((value,index)=>(param[0][index]!=null?param[0][index]:value));
-                }else if(isObject(param[0])){
-                    param = periodValue.map((value,index)=>(param[0][periodKey[index]]!=null?param[0][periodKey[index]]:value));
+                if(isArray(params[0])){
+                    params = periodValue.map((value,index)=>(params[0][index]!=null?params[0][index]:value));
+                }else if(isObject(params[0])){
+                    params = periodValue.map((value,index)=>(params[0][periodKey[index]]!=null?params[0][periodKey[index]]:value));
                 }
-                if(param.length==1&&isString(param[0])){
-                    let matchs1 = param[0].match(/(\d{1,4})[\-\/](\d{1,2})[\-\/](\d{1,2})([\sT](\d{1,2})?:(\d{1,2})?(:(\d{1,2}))?(\.(\d{1,3}))?)?/);
-                    let matchs2 = param[0].match(/(\d{1,2})[\-\/](\d{1,2})[\-\/](\d{3,4})([\sT](\d{1,2})?:(\d{1,2})?(:(\d{1,2}))?(\.(\d{1,3}))?)?/);
-                    let matchs3 = param[0].match(/^([12]\d{3})(\d{2})(\d{2})(\d{2})?(\d{2})?(\d{2})?(\d{1,3})?/);
+                if(params.length==1&&isString(params[0])){
+                    let matchs1 = params[0].match(/(\d{1,4})[\-\/](\d{1,2})[\-\/](\d{1,2})([\sT](\d{1,2})?:(\d{1,2})?(:(\d{1,2}))?(\.(\d{1,3}))?)?/);
+                    let matchs2 = params[0].match(/(\d{1,2})[\-\/](\d{1,2})[\-\/](\d{3,4})([\sT](\d{1,2})?:(\d{1,2})?(:(\d{1,2}))?(\.(\d{1,3}))?)?/);
+                    let matchs3 = params[0].match(/^([12]\d{3})(\d{2})(\d{2})(\d{2})?(\d{2})?(\d{2})?(\d{1,3})?/);
                     if(matchs1&&!matchs2){
-                        param = [1,2,3,5,6,8,10].map(function(i,index){
+                        params = [1,2,3,5,6,8,10].map(function(i,index){
                             return +(matchs1[i]||periodValue[index]);
                         });
                     }else if(matchs2){
-                        param = [3,1,2,5,6,8,10].map(function(i,index){
+                        params = [3,1,2,5,6,8,10].map(function(i,index){
                             return +(matchs2[i]||periodValue[index]);
                         });
                     }else if(matchs3){
-                        param = [1,2,3,4,5,6,7].map(function(i,index){
+                        params = [1,2,3,4,5,6,7].map(function(i,index){
                             return +(matchs3[i]||periodValue[index]);
                         });
                     }
                 }
                 // 参数修复
-                if(param.length>=3){
-                    param[1]--;
+                if(params.length>=3){
+                    params[1]--;
                 }
                 // 字符串输入做字段范围校验（month 1-12, day 1-31, hour 0-23, minute/second 0-59）
-                const isInvalid = isString(argu[0])&&(
-                    (param[1]<0||param[1]>11)||
-                    (param[2]<1||param[2]>31)||
-                    (param[3]<0||param[3]>23)||
-                    (param[4]<0||param[4]>59)||
-                    (param[5]<0||param[5]>59)
-                );
+                const isInvalid = isString(argu[0])&&(function(){
+                    const [year = 0, month = 0, day = 0, hour = 0, minute = 0, second = 0, millisecond = 0] = params;
+                    const date = new Date(year, month, day, hour, minute, second, millisecond);
+                    return (
+                        date.getFullYear() !== year ||
+                        date.getMonth() !== month ||
+                        date.getDate() !== day ||
+                        date.getHours() !== hour ||
+                        date.getMinutes() !== minute ||
+                        date.getSeconds() !== second ||
+                        date.getMilliseconds() !== millisecond
+                    );
+                })();
                 if(isInvalid){
                     this._date = new Date(''); // 无效输入，置 Invalid Date
                 }else{
                     // 初始化
-                    this._date = new Date(...param);
-                    if(param.length>=2&&isNumber(param[0])&&param[0]<100){
-                        this._date.setFullYear(param[0]);
+                    this._date = new Date(...params);
+                    if(params.length>=2&&isNumber(params[0])&&params[0]<100){
+                        this._date.setFullYear(params[0]);
                     }
                 }
             }
