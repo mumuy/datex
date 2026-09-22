97c023fce96579645d4ed8d670dffac1fff04c19
mumuy <zhenghaole1990@126.com>
2026-09-22 14:12:09 +0800

    updated 添加min和max方法

--- src/module/method/compare.js
+++ src/module/method/compare.js
@@ -1,7 +1,25 @@
 /*
  * 对比方法
 */
+import { isArray} from './utils/type.js';
+
 export default function(datex,proto){
+    Object.assign(datex,{
+        min: function(...params){
+            if(params.length === 1&& isArray(params[0])){
+                params = params[0];
+            }
+            params = params.map(time=>datex(time));
+            return params.reduce((a,b)=>a.isBefore(b)?a:b);
+        },
+        max: function(...params){
+            if(params.length === 1&& isArray(params[0])){
+                params = params[0];
+            }
+            params = params.map(time=>datex(time));
+            return params.reduce((a,b)=>a.isAfter(b)?a:b);
+        }
+    });
 
     Object.assign(proto,{
         isAfter(that,unit = 'timestamp'){
